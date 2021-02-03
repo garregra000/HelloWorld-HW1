e257e8ac829b9eac8351b9b2e6fb3b31dc7bf990
graceg <gracegarretson@gmail.com>
2021-02-03 17:50:27 -0500

    changed my name

--- src/main/java/com/example/helloworld/HelloWorldController.java
+++ src/main/java/com/example/helloworld/HelloWorldController.java
@@ -9,6 +9,6 @@ public class HelloWorldController {
     @RequestMapping("/")
     public String index()
     {
-        return "Hello from ABC Inc. est 2008";
+        return "Hello Grace Garretson";
     }
 }
